35e8ee542c9278cfc601f6fd41dfcd1a91df9110
M <m@mathsistor.com>
2023-12-14 21:25:34 -0500

    Rename module and restructre files

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import pytest
-from todo.models import TodoItem
+from todo.models.pydantic_models import TodoItem
 
 
 def test_todo_item_creation_with_valid_data():

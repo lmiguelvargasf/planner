ef6c8755b39f1b9760c0c8ca6ad2c52e4b14b960
M <lmiguelvargasf@gmail.com>
2024-02-17 06:42:35 -0500

    Add database level default value

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,7 +1,11 @@
+from sqlmodel import Field
+
 from ..core.models import BaseModel
 
 
 class TodoItem(BaseModel, table=True):
     title: str
     description: str | None
-    completed: bool = False
+    completed: bool = Field(
+        default=False, sa_column_kwargs=dict(server_default="FALSE")
+    )

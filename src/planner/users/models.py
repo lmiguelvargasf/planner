a00fc6b9798e71e1745646783c7ae16627f6d00e
M <lmiguelvargasf@gmail.com>
2024-02-18 21:28:22 -0500

    Add fields to User model to make it more generic

--- src/planner/users/models.py
+++ src/planner/users/models.py
@@ -1,3 +1,6 @@
+from datetime import date
+from enum import StrEnum, auto
+
 from pydantic import EmailStr
 from sqlalchemy import false, true
 from sqlmodel import Column, Field, String
@@ -5,13 +8,22 @@ from sqlmodel import Column, Field, String
 from ..core.models import BaseModel
 
 
+class Sex(StrEnum):
+    MALE = auto()
+    FEMALE = auto()
+
+
 class User(BaseModel, table=True):
+    first_name: str | None = Field(nullable=True)
+    middle_name: str | None = Field(nullable=True)
+    last_name: str | None = Field(nullable=True)
+    second_last_name: str | None = Field(nullable=True)
+    date_of_birth: date | None = Field(nullable=True)
+    sex: Sex | None = Field(nullable=True)
     email: EmailStr = Field(
         sa_column=Column(String, unique=True, index=True, nullable=False)
     )
     hashed_password: str | None
-    first_name: str | None = Field(nullable=True)
-    last_name: str | None = Field(nullable=True)
     is_active: bool = Field(
         default=True, nullable=False, sa_column_kwargs=dict(server_default=true())
     )

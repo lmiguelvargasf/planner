41efbe0a4b8b2cb8faf5f94b20dd8bd0e199401b
M <lmiguelvargasf@gmail.com>
2024-02-19 12:55:58 -0500

    Add test for full data

--- tests/users/test_models.py
+++ tests/users/test_models.py
@@ -6,18 +6,18 @@ from planner.users.models import Sex, User
 
 def test_user_creation_minimum_data():
     user = User(email="user@example.com")
-    user_db = User.model_validate(user)
+    db_user = User.model_validate(user)
 
-    assert user_db.first_name is None
-    assert user_db.middle_name is None
-    assert user_db.last_name is None
-    assert user_db.second_last_name is None
-    assert user_db.date_of_birth is None
-    assert user_db.sex is None
-    assert user_db.email == "user@example.com"
-    assert user_db.hashed_password is None
-    assert user_db.is_active is True
-    assert user_db.is_superuser is False
+    assert db_user.first_name is None
+    assert db_user.middle_name is None
+    assert db_user.last_name is None
+    assert db_user.second_last_name is None
+    assert db_user.date_of_birth is None
+    assert db_user.sex is None
+    assert db_user.email == "user@example.com"
+    assert db_user.hashed_password is None
+    assert db_user.is_active is True
+    assert db_user.is_superuser is False
 
 
 def test_user_email_validation():
@@ -36,11 +36,11 @@ def test_user_sex_validation():
     female_user = User(email="female@example.com", sex=Sex.FEMALE)
     user_with_invalid_sex = User(email="invalid@example.com", sex="invalid")
 
-    male_user_db = User.model_validate(male_user)
-    female_user_db = User.model_validate(female_user)
+    db_male_user = User.model_validate(male_user)
+    db_female_user = User.model_validate(female_user)
 
-    assert male_user_db.sex == Sex.MALE
-    assert female_user_db.sex == Sex.FEMALE
+    assert db_male_user.sex == Sex.MALE
+    assert db_female_user.sex == Sex.FEMALE
 
     with pytest.raises(ValueError, match="sex"):
         User.model_validate(user_with_invalid_sex)
@@ -52,9 +52,36 @@ def test_user_date_of_birth_validation():
         email="invalid@example.com", date_of_birth="not a date"
     )
 
-    user_db = User.model_validate(user)
+    db_user = User.model_validate(user)
 
-    assert user_db.date_of_birth == date(1990, 1, 1)
+    assert db_user.date_of_birth == date(1990, 1, 1)
 
     with pytest.raises(ValueError, match="date_of_birth"):
         User.model_validate(user_with_invalid_date_of_birth)
+
+
+def test_user_creation_full_data():
+    user = User(
+        first_name="John",
+        middle_name="James",
+        last_name="Smith",
+        second_last_name="Wilson",
+        date_of_birth=date(1980, 5, 20),
+        sex=Sex.MALE,
+        email="john.smith@example.com",
+        hashed_password="password",
+        is_active=False,
+        is_superuser=True,
+    )
+    db_user = User.model_validate(user)
+
+    assert db_user.first_name == "John"
+    assert db_user.middle_name == "James"
+    assert db_user.last_name == "Smith"
+    assert db_user.second_last_name == "Wilson"
+    assert db_user.date_of_birth == date(1980, 5, 20)
+    assert db_user.sex == Sex.MALE
+    assert db_user.email == "john.smith@example.com"
+    assert db_user.hashed_password.get_secret_value() == "password"
+    assert db_user.is_active is False
+    assert db_user.is_superuser is True

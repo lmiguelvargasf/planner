d8000d8025d9772a505f67159c892cb7634397a9
M <lmiguelvargasf@gmail.com>
2024-02-22 08:19:43 -0500

    Implement get_db_session and improve docstring of inject_db_session

--- src/planner/database.py
+++ src/planner/database.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -14,10 +15,22 @@ async_session = async_sessionmaker(bind=async_engine, expire_on_commit=False)
 
 async def inject_db_session() -> AsyncGenerator[AsyncSession, None]:
     """
-    Provide a database session for asynchronous web applications, designed for
-    seamless integration with dependency injection systems. This function is
-    crafted for use with FastAPI's `Depends`, but it should also work well
-    with other async frameworks.
+    Provide an asynchronous database session.
+
+    Designed first for FastAPI's `Depends` to facilitate dependency injection,
+    it should be suitable and functional in other asynchronous contexts.
+
+    Yields:
+        An async database session.
+    """
+    async with async_session() as session:
+        yield session
+
+
+@asynccontextmanager
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Provide a context manager for async database sessions.
 
     Yields:
         An async database session.

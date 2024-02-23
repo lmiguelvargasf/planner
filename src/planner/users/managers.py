f0dd1a60a583ca2edb4fe3b02929f38d14523151
M <lmiguelvargasf@gmail.com>
2024-02-23 00:02:42 -0500

    Implement method to create users in manager

---
+++ src/planner/users/managers.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from .models import User
+
+
+@dataclass(repr=False, eq=False)
+class UserManager:
+    session: AsyncSession
+
+    async def create(self, user: User) -> User:
+        self.session.add(user)
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user

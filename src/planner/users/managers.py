4295717ff9a3c5e79e222296c8182910fbb5de59
M <lmiguelvargasf@gmail.com>
2024-02-23 19:14:06 -0500

    Implement method to update a User

--- src/planner/users/managers.py
+++ src/planner/users/managers.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from uuid import UUID
 
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from .models import User
@@ -18,3 +19,18 @@ class UserManager:
 
     async def get_by_uuid(self, *, uuid: UUID) -> User | None:
         return await self.session.get(User, uuid)
+
+    async def update(self, *, uuid: UUID, user: User) -> User:
+        db_user = await self._get_by_uuid(uuid)
+        dumped_user = db_user.model_dump() | user.model_dump()
+        db_user.sqlmodel_update(dumped_user)
+        self.session.add(db_user)
+        await self.session.commit()
+        await self.session.refresh(db_user)
+        return db_user
+
+    async def _get_by_uuid(self, uuid: UUID) -> User:
+        query = select(User).where(User.uuid == uuid)
+        result = await self.session.exec(query)
+        db_user = result.one()
+        return db_user

f0c3e8d61db1a8f000d293dd794d63e280e5f105
M <lmiguelvargasf@gmail.com>
2024-02-25 15:59:08 -0500

    Complete testing for user manager

--- tests/users/test_managers.py
+++ tests/users/test_managers.py
@@ -1,11 +1,13 @@
 import pytest
 import pytest_asyncio
 from planner.users.managers import UserManager
-from planner.users.models import Sex, User, UserCreate
-from sqlalchemy.exc import IntegrityError
+from planner.users.models import Sex, User, UserCreate, UserUpdate
+from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 EMAIL = "user@example.com"
+FIRST_NAME = "John"
+LAST_NAME = "Smith"
 
 
 @pytest.fixture
@@ -18,9 +20,9 @@ def validated_basic_user():
 @pytest.fixture
 def validated_complete_user():
     user = UserCreate(
-        first_name="John",
+        first_name=FIRST_NAME,
         middle_name="Doe",
-        last_name="Smith",
+        last_name=LAST_NAME,
         second_last_name="Johnson",
         date_of_birth="1990-01-01",
         sex=Sex.MALE,
@@ -90,3 +92,100 @@ async def test_create_user_with_unique_email(session: AsyncSession, db_basic_use
 
     with pytest.raises(IntegrityError, match="unique constraint"):
         await manager.create(validated_user)
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_uuid(session: AsyncSession, db_basic_user):
+    manager = UserManager(session)
+    user = await manager.get_by_uuid(uuid=db_basic_user.uuid)
+    assert user == db_basic_user
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_uuid_not_found(session: AsyncSession):
+    manager = UserManager(session)
+    uuid = "00000000-0000-0000-0000-000000000000"
+
+    with pytest.raises(NoResultFound, match="No row was found"):
+        await manager.get_by_uuid(uuid=uuid)
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_email(session: AsyncSession, db_basic_user):
+    manager = UserManager(session)
+    user = await manager.get_by_email(email=EMAIL)
+    assert user == db_basic_user
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_email_not_found(session: AsyncSession):
+    manager = UserManager(session)
+
+    with pytest.raises(NoResultFound, match="No row was found"):
+        await manager.get_by_email(email=EMAIL)
+
+
+@pytest.mark.asyncio
+async def test_patch_user(session: AsyncSession, db_basic_user):
+    manager = UserManager(session)
+    user = UserUpdate(first_name=FIRST_NAME, last_name=LAST_NAME)
+
+    updated_user = await manager.patch(uuid=db_basic_user.uuid, user=user)
+
+    assert updated_user.uuid == db_basic_user.uuid
+    assert updated_user.first_name == FIRST_NAME
+    assert updated_user.last_name == LAST_NAME
+
+
+@pytest.mark.asyncio
+async def test_patch_email_is_none(session: AsyncSession, db_basic_user):
+    manager = UserManager(session)
+    user = UserUpdate(email=None)
+
+    with pytest.raises(IntegrityError, match="violates not-null constraint"):
+        await manager.patch(uuid=db_basic_user.uuid, user=user)
+
+
+@pytest.mark.asyncio
+async def test_patch_user_with_unique_email(session: AsyncSession, db_basic_user):
+    manager = UserManager(session)
+
+    # create another user
+    another_email = "another@example.com"  # noqa: N806
+    user = UserCreate(email=another_email)
+    validated_user = User.model_validate(user)
+    await manager.create(validated_user)
+
+    # update user with email of another user
+    user = UserUpdate(email=another_email)
+
+    with pytest.raises(IntegrityError, match="unique constraint"):
+        await manager.patch(uuid=db_basic_user.uuid, user=user)
+
+
+@pytest.mark.asyncio
+async def test_patch_user_not_found(session: AsyncSession):
+    manager = UserManager(session)
+    user = UserUpdate(first_name=FIRST_NAME, last_name=LAST_NAME)
+    uuid = "00000000-0000-0000-0000-000000000000"
+
+    with pytest.raises(NoResultFound, match="No row was found"):
+        await manager.patch(uuid=uuid, user=user)
+
+
+@pytest.mark.asyncio
+async def test_delete_user(session: AsyncSession, db_basic_user):
+    manager = UserManager(session)
+    await manager.delete(uuid=db_basic_user.uuid)
+
+    with pytest.raises(NoResultFound, match="No row was found"):
+        await manager.get_by_uuid(uuid=db_basic_user.uuid)
+
+
+@pytest.mark.asyncio
+async def test_delete_user_not_found(session: AsyncSession):
+    manager = UserManager(session)
+    uuid = "00000000-0000-0000-0000-000000000000"
+
+    with pytest.raises(NoResultFound, match="No row was found"):
+        await manager.delete(uuid=uuid)

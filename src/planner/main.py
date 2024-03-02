9c21db51e0c0c0205e77f48d65f5484a0022b162
M <lmiguelvargasf@gmail.com>
2024-03-01 21:02:29 -0500

    Define exception handler for BaseError

--- src/planner/main.py
+++ src/planner/main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 
+from .core.exceptions import BaseError
 from .users.routers import router as users_router
 
 app = FastAPI()
 app.include_router(users_router)
+
+
+@app.exception_handler(BaseError)
+async def base_error_handler(request: Request, error: BaseError) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"message": error.message},
+    )
 
 
 @app.get("/", include_in_schema=False)

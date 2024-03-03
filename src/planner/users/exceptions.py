52f7c531c1fb9e12c220ba87b662dd2056b32e69
M <lmiguelvargasf@gmail.com>
2024-03-03 07:56:24 -0500

    Add enum key/value for email required

--- src/planner/users/exceptions.py
+++ src/planner/users/exceptions.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
-from enum import Enum
+from enum import StrEnum
 
 from ..core.exceptions import BaseError
 
 
-class UserErrorMessage(Enum):
+class UserErrorMessage(StrEnum):
     DUPLICATE_EMAIL = "A user with this email already exists."
     NOT_FOUND_BY_UUID = "The user for the provided UUID does not exist."
     NOT_FOUND_BY_EMAIL = "The user for the provided email does not exist."
+    EMAIL_REQUIRED = "The user's email is required."
 
 
 @dataclass

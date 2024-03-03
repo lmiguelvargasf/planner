34154601039bdc1c61fd72e97f2ce93871a73a4f
M <lmiguelvargasf@gmail.com>
2024-03-03 18:36:18 -0500

    Pass message as a parameter of exception

--- src/planner/core/exceptions.py
+++ src/planner/core/exceptions.py
@@ -7,3 +7,6 @@ class BaseError(ABC, Exception):
     """Abstract base class for custom exceptions."""
 
     message: str
+
+    def __post_init__(self) -> None:
+        super().__init__(self.message)

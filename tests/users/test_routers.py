ecb06f4aeb503c04dec1039b61cea49e22b2c1ea
M <m@mathsistor.com>
2024-04-09 22:18:31 -0700

    Avoid hard-coding routes

--- tests/users/test_routers.py
+++ tests/users/test_routers.py
@@ -11,12 +11,13 @@ user_manager_mock = AsyncMock()
 validated_user = User.model_validate(DB_USER)
 user_manager_mock.create = AsyncMock(return_value=validated_user)
 user_manager_mock.get_by_uuid = AsyncMock(return_value=validated_user)
+user_manager_mock.get_by_email = AsyncMock(return_value=validated_user)
 app.dependency_overrides[get_user_manager] = lambda: user_manager_mock
 
 
 async def test_create_user(client: AsyncClient):
     response = await client.post(
-        "/users/",
+        app.url_path_for("create_user"),
         content=DB_USER.model_dump_json(exclude_unset=True),
     )
     expected_user = UserRead.model_validate(DB_USER)
@@ -27,7 +28,20 @@ async def test_create_user(client: AsyncClient):
 
 
 async def test_get_user_by_uuid(client: AsyncClient):
-    response = await client.get(f"/users/{DB_USER.uuid}")
+    response = await client.get(
+        app.url_path_for("read_user_by_uuid", user_uuid=DB_USER.uuid)
+    )
+    expected_user = UserRead.model_validate(DB_USER)
+    actual_user = UserRead.model_validate(response.json())
+
+    assert response.status_code == status.HTTP_200_OK
+    assert expected_user == actual_user
+
+
+async def test_get_user_by_email(client: AsyncClient):
+    response = await client.get(
+        app.url_path_for("read_user_by_email", email=DB_USER.email)
+    )
     expected_user = UserRead.model_validate(DB_USER)
     actual_user = UserRead.model_validate(response.json())
 

6ad93234e5caf56e5efc115c0e0a0dd7ae8fc87f
M <m@mathsistor.com>
2024-04-10 07:33:17 -0700

    Implement test for delete endpoint

--- tests/users/test_routers.py
+++ tests/users/test_routers.py
@@ -4,23 +4,29 @@ from fastapi import status
 from httpx import AsyncClient
 from planner.main import app
 from planner.users.dependencies import get_user_manager
-from planner.users.models import User, UserRead
+from planner.users.models import User, UserCreate, UserRead, UserUpdate
 
-DB_USER = User(email="user@example.com")
+USER_CREATE = UserCreate(email="user@example.com")
+USER = User.model_validate(USER_CREATE)
 user_manager_mock = AsyncMock()
-validated_user = User.model_validate(DB_USER)
+validated_user = User.model_validate(USER)
 user_manager_mock.create = AsyncMock(return_value=validated_user)
 user_manager_mock.get_by_uuid = AsyncMock(return_value=validated_user)
 user_manager_mock.get_by_email = AsyncMock(return_value=validated_user)
+USER_UPDATE = UserUpdate(email="updated@example.com")
+UPDATED_USER = User.model_validate(
+    USER.model_dump() | USER_UPDATE.model_dump(exclude_unset=True)
+)
+user_manager_mock.patch = AsyncMock(return_value=UPDATED_USER)
 app.dependency_overrides[get_user_manager] = lambda: user_manager_mock
 
 
 async def test_create_user(client: AsyncClient):
     response = await client.post(
         app.url_path_for("create_user"),
-        content=DB_USER.model_dump_json(exclude_unset=True),
+        content=USER_CREATE.model_dump_json(exclude_unset=True),
     )
-    expected_user = UserRead.model_validate(DB_USER)
+    expected_user = UserRead.model_validate(USER)
     actual_user = UserRead.model_validate(response.json())
 
     assert response.status_code == status.HTTP_201_CREATED
@@ -29,9 +35,9 @@ async def test_create_user(client: AsyncClient):
 
 async def test_get_user_by_uuid(client: AsyncClient):
     response = await client.get(
-        app.url_path_for("read_user_by_uuid", user_uuid=DB_USER.uuid)
+        app.url_path_for("read_user_by_uuid", user_uuid=USER.uuid)
     )
-    expected_user = UserRead.model_validate(DB_USER)
+    expected_user = UserRead.model_validate(USER)
     actual_user = UserRead.model_validate(response.json())
 
     assert response.status_code == status.HTTP_200_OK
@@ -40,10 +46,28 @@ async def test_get_user_by_uuid(client: AsyncClient):
 
 async def test_get_user_by_email(client: AsyncClient):
     response = await client.get(
-        app.url_path_for("read_user_by_email", email=DB_USER.email)
+        app.url_path_for("read_user_by_email", email=USER.email)
     )
-    expected_user = UserRead.model_validate(DB_USER)
+    expected_user = UserRead.model_validate(USER)
     actual_user = UserRead.model_validate(response.json())
 
     assert response.status_code == status.HTTP_200_OK
     assert expected_user == actual_user
+
+
+async def test_patch_user(client: AsyncClient):
+    response = await client.patch(
+        app.url_path_for("patch_user", user_uuid=USER.uuid),
+        content=USER_UPDATE.model_dump_json(exclude_unset=True),
+    )
+    expected_user = UserRead.model_validate(UPDATED_USER)
+    actual_user = UserRead.model_validate(response.json())
+
+    assert response.status_code == status.HTTP_200_OK
+    assert expected_user == actual_user
+
+
+async def test_delete_user(client: AsyncClient):
+    response = await client.delete(app.url_path_for("delete_user", user_uuid=USER.uuid))
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
